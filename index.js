4a48c4d1c812f29f6f96db2700c37dc8e9238e5c
Jannis R <mail@jannisr.de>
2016-05-20 02:44:27 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict'
 
-// todo
+const search = require('vbb-find-stations')
+
+const find = (query) => new Promise((yay, nay) => {
+	let relevance = -Infinity
+	let station   = null
+	search(query, search.fuzzy).on('error', nay)
+	.on('data', (s) => {
+		if (s.relevance > relevance) {
+			station = s
+			relevance = s.relevance
+		}
+	})
+	.on('end', () => yay(station))
+})
+
+module.exports = find
